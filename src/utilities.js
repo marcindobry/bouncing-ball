29e4a69703f0ac479dd268b152e9f24a744003dc
Marcin Dobrowolski <marcindobry@hotmail.com>
2016-03-07 21:58:33 +0000

    Add object argument to #dimensions

--- src/utilities.js
+++ src/utilities.js
@@ -1,4 +1,10 @@
-export function dimensions() {
+export function dimensions($object = null) {
+  if($object !== null) {
+    return {
+      width: $object.clientWidth,
+      height: $object.clientHeight,
+    }
+  }
   return {
     width: window.innerWidth || document.body.clientWidth,
     height: window.innerHeight || document.body.clientHeight
