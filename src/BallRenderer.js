f013e4495360c7159bbe5bb13759f0f2c7b24c0f
Marcin Dobrowolski <marcindobry@hotmail.com>
2016-03-08 21:24:39 +0000

    Move width and height to ball renderer

--- src/BallRenderer.js
+++ src/BallRenderer.js
@@ -1,10 +1,32 @@
+const RADIUS = 5;
+
 export default class BallRenderer {
   constructor(ball) {
     this.ball = ball;
     this.$ballElement = $("<div/>", {
       "class": "ball",
-      style: `background-color: red; top: ${ball.y}px; left: ${ball.x}px;`
+      style: `background-color: red;
+        top: ${this.top()}px;
+        left: ${this.left()}px;
+        width: ${this.diameter()}px;
+        height: ${this.diameter()}px;`
     });
+  }
+
+  top() {
+    return this.ball.y - this.radius();
+  }
+
+  left() {
+    return this.ball.x - this.radius();
+  }
+
+  radius() {
+    return RADIUS;
+  }
+
+  diameter() {
+    return this.radius() * 2;
   }
 
   render() {
