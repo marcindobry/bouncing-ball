8c3e4ed295dd2f816300c0246145c8589b1972ca
Marcin Dobrowolski <marcindobry@hotmail.com>
2016-03-09 21:20:52 +0000

    First draft of gravity

--- src/Ball.js
+++ src/Ball.js
@@ -2,12 +2,35 @@ export default class Ball {
   constructor(x, y) {
     this.x = x;
     this.y = y;
-    this.speed = Math.random();
-    this.angle = Math.random() * 2 * Math.PI
+    this.speed = (Math.random() + 1) * 2;
+    this.angle = (Math.random() * 2 * Math.PI) - Math.PI;
   }
 
   move() {
-    this.x += Math.cos(this.angle) * this.speed;
-    this.y += Math.sin(this.angle) * this.speed;
+    let vector = this.vector();
+    this.x -= vector.x;
+    this.y -= vector.y;
+    this.applyGravity();
+    this.applyBorders();
+  }
+
+  applyGravity() {
+    let gravityForce = 0.05;
+    let vector = this.vector();
+    let fnorm = vector.y - gravityForce;
+    this.speed = Math.sqrt(Math.pow(vector.x, 2) + Math.pow(fnorm, 2))
+    let anglePrime = Math.asin(fnorm / this.speed);
+    if((this.angle <= (Math.PI / 2)) && (this.angle >= (-Math.PI / 2))) {
+      this.angle = anglePrime;
+    } else {
+      this.angle = Math.PI - anglePrime;
+    }
+  }
+
+  vector() {
+    return {
+      x: Math.cos(this.angle) * this.speed,
+      y: Math.sin(this.angle) * this.speed
+    }
   }
 }
